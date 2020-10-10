816e875e6fc301c955781dc97a25a98085897152
Aichen Yao <aicheny@andrew.cmu.edu>
2020-10-10 04:52:59 -0400

    working with address/t/s/b and modified myCache structure

--- csim.c
+++ csim.c
@@ -8,13 +8,6 @@
 #include<string.h> 
 #define LONG_NUMBER 64
 
-typedef struct{
-    long hits;
-    long misses;
-    long evictions;
-    long dirty_bytes;
-    long dirty_evictions;
-}csim_stats_t;
 
 typedef struct{
     long validBit;
@@ -28,24 +21,52 @@ line_struct **myCache = NULL;
 
 char traceFile[1000];
 char buffer[1000];
-long getTag(long address, long s, long b);
 
-void loadAndStore(long address);
+long t, s, b, S, E, help_mode, verbose_mode, hits, misses, evictions;
+long dirty_bytes, dirty_evictions;
 
+long findSet(long address, long s, long b)
+//Use bit manipulations to locate the set (row) we want to search on
+//in the cache. The output of this function is the setIndex.
+{
+    long mask = (1 << s) - 1;
+    long base = address >> b;
+    return base & mask;
+}
+
+long getTag(long address, long t, long s, long b)
+//Use bit manipulations to extract the tag bits from the address.
+//We will use this to determine pinpoint the block in the target set (row)
+{
+    long base = address >> (b + s);
+    long mask = (1 << t) - 1;
+    return base & mask;
+}
+
+void loadAndStore(long address, long t, long s, long b, long E)
+{
+    long addressSet = findSet(address, s, b);
+    long addressTag = getTag(address, t, s, b);
+    for (int j = 0; j < E; j ++)
+    {
+        line_struct* current = myCache[addressSet][j];
+        if (current->validBit == 1)
+            {
+                if (current->tagBits == addressTag)
+                {
+                    hits += 1;
+                }
+            }
+        misses += 1;
+    }
+}
 
 int main(int argc, char* argv[])
 {
     long opt;
-    long hits = 0;
-    int misses = 0;
-    long evictions = 0;
-    long s;
-    long S;
-    long E;
-    long b;
-    long t;
-    long help_mode;
-    long verbose_mode;
+    long temp;
+    char type;
+    long address;
     while ((opt = (getopt(argc, argv, "hvs:E:b:t:"))) != -1)
     {
         switch(opt)
@@ -60,7 +81,7 @@ int main(int argc, char* argv[])
                             break;
             case 'b':b = atoi(optarg);
                             break;
-            case 't':t = atoi(optarg);
+            case 't':strcpy(traceFile, optarg);
                             break;
         }
     }
@@ -92,6 +113,22 @@ int main(int argc, char* argv[])
             //the validBit of all lines are 0 at first, "cold misses"
         }
     }
+    while(fgets(buffer,1000,trace))  //get a whole line, cite C library
+    {   //parse the trace files and get the address in hex to pass
+        //on to loadAndStore()
+        sscanf(buffer," %c, %ld, %ld", &type, &address, &temp);
+        switch(type)
+        {
+            case 'L':loadAndStore(address, t, s, b, E);
+            case 'S':loadAndStore(address, t, s, b, E);
+                                    break;
+        }
+    }
+    for (int i = 0; i < S; i++)
+    {   //free each row of myCache
+        free(myCache[i]);
+    }  
     free(myCache);
     fclose(trace);
     return 0;
+}
